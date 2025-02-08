7321cf0317c067ec668242774d8a8d7a2fa098ff
Grydr <jalucahyoseno@gmail.com>
2025-02-08 11:11:01 +0700

    i'm dum dum (only need to multiply it with -1) skull emoji

--- bytemanipulate.cpp
+++ bytemanipulate.cpp
@@ -1,44 +1,51 @@
-#include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <sys/types.h>
 
+// Thrusters
 const int back_left_thruster = 477;
 const int middle_left_thruster = -350;
 const int top_left_thruster = -244;
-const int back_right_thruster = 500;
+const int back_right_thruster = 350;
 const int middle_right_thruster = 500;
 const int top_right_thruster = 500;
 
-// flags
+// Flags
 const int pid = 1;
-const int takeBall = 1;
+const int takeBall = 0;
 const int dropBall = 1;
 
-// const char *thruster_names[] = {"back_left_thruster", "middle_left_thruster", "top_left_thruster", "back_right_thruster", "middle_right_thruster", "top_right_thruster"};
+/*
+   data[0-5] 8 bits for thrusters
+   data[6] 6 bits for thrusters msb (overflow) + 2 bits for signBit
+   data[7] 4 bits for the remaining signBit + 3 bits for flags
+*/
 
-void storeThruster(uint8_t *data, int value, size_t pos) {
+void storeThruster(uint8_t *data, int value, int pos) {
    bool isNegative = (value < 0);
    int absValue = std::abs(value);
 
-   data[pos] = static_cast<uint8_t>(absValue & 0xFF); // High byte (8 bits)
+   data[pos] = static_cast<uint8_t>(absValue & 0xFF);
 
    uint8_t msb = ((absValue >> 8) & 0x01) << pos;
-   data[6] |= msb; // Low byte (1 bits)
+   data[6] |= msb;
 
-   uint8_t signBit = isNegative ? 1 : 0;
+   uint8_t signBit = isNegative ? 0x01 : 0x00;
    if (pos < 2) {
       data[6] |= signBit << (pos + 6);
    } else {
       data[7] |= signBit << (pos - 2);
+      // uint8_t temp = signBit << (pos - 2);
+      // std::cout << "Reach here, signBit is: 0x" << std::hex << static_cast<uint>(temp) << std::endl;
+      // data[7] = 81;
    }
 }
 
-void readThruster(uint8_t *data, std::string thrusterName, size_t pos) {
-   uint16_t msb = (data[6] >> pos) & 0x01;
-   uint16_t value = data[pos] | msb << 8;
+void readThruster(uint8_t *data, std::string thrusterName, int pos) {
+   uint16_t msb = ((data[6] >> pos) & 0x01) << 8;
+   int value = data[pos] | msb;
 
    int signBit;
    if (pos < 2) {
@@ -47,34 +54,47 @@ void readThruster(uint8_t *data, std::string thrusterName, size_t pos) {
       signBit = (data[7] >> (pos - 2)) & 0x01;
    }
 
-   value = signBit ? -value : value;
+   if (signBit) {
+      value = -1 * value;
+   }
    std::cout << thrusterName << ": " << std::dec << value << std::endl;
 }
 
-void storeFlags(uint8_t *data) {
-   data[7] |= (pid ? 0x10 : 0x00);
-   data[7] |= (takeBall ? 0x20 : 0x00);
-   data[7] |= (dropBall ? 0x40 : 0x00);
+void storeFlags(uint8_t *data, bool flagVal, int pos) {
+   // data[7] |= (pid ? 0x10 : 0x00);
+   // data[7] |= (takeBall ? 0x20 : 0x00);
+   // data[7] |= (dropBall ? 0x40 : 0x00);
+
+   if (pos > 2) {
+      fprintf(stderr, "ERROR: flags input position exceed array size\n");
+      exit(1);
+   }
+   data[7] |= static_cast<uint8_t>(flagVal) << (pos + 4);
 }
 
-void readFlags(uint8_t *data) {
-   bool pidVal = data[7] & 0x10;
-   bool takeVal = data[7] & 0x20;
-   bool dropVal = data[7] & 0x40;
+void readFlags(uint8_t *data, std::string flagName, int pos) {
+   // bool pidVal = data[7] & 0x10;
+   // bool takeVal = data[7] & 0x20;
+   // bool dropVal = data[7] & 0x40;
 
-   std::cout << "PID: " << pidVal << "\nTake: " << takeVal << "\nDrop: " << dropVal << "\n";
+   if (pos > 2) {
+      fprintf(stderr, "ERROR: flags read position exceed array size\n");
+      exit(1);
+   }
+   bool value = (data[7] >> (pos + 4)) & 0x01;
+   std::cout << flagName << ": " << value << std::endl;
 }
 
 int main() {
    uint8_t data[8] = {0}; // 0-5 LSB, 6 MSB, 7 Flags
 
    // thrusters
-   storeThruster(data, back_left_thruster, 0);    // Bytes 0-1
-   storeThruster(data, middle_left_thruster, 1);  // Bytes 2-3
-   storeThruster(data, top_left_thruster, 2);     // Bytes 4-5
-   storeThruster(data, back_right_thruster, 3);   // Bytes 6-7
-   storeThruster(data, middle_right_thruster, 4); // Bytes 8-9
-   storeThruster(data, top_right_thruster, 5);    // Bytes 10-11
+   storeThruster(data, back_left_thruster, 0);    // Bytes 0
+   storeThruster(data, middle_left_thruster, 1);  // Bytes 1
+   storeThruster(data, top_left_thruster, 2);     // Bytes 2
+   storeThruster(data, back_right_thruster, 3);   // Bytes 3
+   storeThruster(data, middle_right_thruster, 4); // Bytes 4
+   storeThruster(data, top_right_thruster, 5);    // Bytes 5
 
    // print the packed data per bytes
    std::cout << "Bytes data:\n";
@@ -85,19 +105,23 @@ int main() {
 
    // print the values of thrusters
    std::cout << "Thruster values:\n";
-   readThruster(data, "back_left_thruster", 0);    // Bytes 0-1
-   readThruster(data, "middle_left_thruster", 1);  // Bytes 2-3
-   readThruster(data, "top_left_thruster", 2);     // Bytes 4-5
-   readThruster(data, "back_right_thruster", 3);   // Bytes 6-7
-   readThruster(data, "middle_right_thruster", 4); // Bytes 8-9
-   readThruster(data, "top_right_thruster", 5);    // Bytes 10-11
+   readThruster(data, "back_left_thruster", 0);    // Bytes 0
+   readThruster(data, "middle_left_thruster", 1);  // Bytes 1
+   readThruster(data, "top_left_thruster", 2);     // Bytes 2
+   readThruster(data, "back_right_thruster", 3);   // Bytes 3
+   readThruster(data, "middle_right_thruster", 4); // Bytes 4
+   readThruster(data, "top_right_thruster", 5);    // Bytes 5
    std::cout << std::endl;
 
-   storeFlags(data);
+   storeFlags(data, pid, 0);
+   storeFlags(data, takeBall, 1);
+   storeFlags(data, dropBall, 2);
 
    // print flags
    std::cout << "flags:\n";
-   readFlags(data);
+   readFlags(data, "PID", 0);
+   readFlags(data, "Take Ball", 1);
+   readFlags(data, "Drop Ball", 2);
 
    return 0;
 }
